7efa8c6c13f5204211ea32c357da22f649ebe2ac
Victor Bosch <boschborno@gmail.com>
2017-01-15 13:51:56 -0400

    Anadi informacion acerca del main

--- Tarea1/main.m
+++ Tarea1/main.m
@@ -1,3 +1,16 @@
+%main.m
+%--------------------------------------------------------------------------
+%Victor Bosch 13-10169
+%Programa para la resolucion del flujo de carga utilizando el metodo de Gauss-Seidel
+%
+%Este programa consiste de 8 archivos distintos que tienen que estar dentro
+%de la misma carpeta para que corra correctamente. Los archivos son:
+%main.m, Input.m, GaussSiter.m ,Flujos_De_Potencia.m, Vpq.m, Qpv.m, Pik.m, Qik.m
+%
+%El ingreso de los datos se hace en el archivo Input.m
+%
+%--------------------------------------------------------------------------
+
 clear
 
 Input
