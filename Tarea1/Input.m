ca552bde9b2e781e9b02491d809183f6d98282ba
Victor Bosch <boschborno@gmail.com>
2017-01-18 09:59:34 -0400

    nada intersante

--- Tarea1/Input.m
+++ Tarea1/Input.m
@@ -33,15 +33,15 @@ datoslineas = [  1    2   0.01    0.20      0.05        0.05 ;
              %#barra Codigo Voltaje  Angulo  Pgen  Qgen    Pcar     Qcar  Qmin Qmax
 
 %Caso cuando la barra 2 genera potencia
-% datosbarras = [ 1     0       1        0     0       0     0        0    NaN  NaN;
-%                 2     1     1.01       0     0.9     0     0        0    NaN  NaN;
-%                 3     2       1        0     0       0    1.5     0.75   NaN  NaN];
+datosbarras = [ 1     0       1        0     0       0     0        0    NaN  NaN;
+                2     1     1.01       0     0.9     0     0        0    NaN  NaN;
+                3     2       1        0     0       0    1.5     0.75   NaN  NaN];
 
 %Caso cuando la barra 2 consume 0.2 de potencia
 
-datosbarras = [ 1     0       1        0     0       0     0         0    NaN  NaN;
-                2     1     1.01       0     0       0    0.2        0    NaN  NaN;
-                3     2       1        0     0       0    1.5     0.75    NaN  NaN];
+% datosbarras = [ 1     0       1        0     0       0     0         0    NaN  NaN;
+%                 2     1     1.01       0     0       0    0.2        0    NaN  NaN;
+%                 3     2       1        0     0       0    1.5     0.75    NaN  NaN];
 
 %Codigos:
 %0->Slack
